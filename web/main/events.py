7d61d9905f3b8a0fb3547e7e282360348b803403
Theo <theokobb@Theos-Air.fios-router.home>
2018-01-02 14:04:03 -0500

    fish gunicorn setup

--- web/main/events.py
+++ web/main/events.py
@@ -1,5 +1,13 @@
+from flask_socketio import send
+
 from main import io
+
 
 @io.on('connect')
 def connect():
-    print('connected')
+    pass
+
+@io.on('ping')
+def ping():
+    send('pong', json='true')
+
