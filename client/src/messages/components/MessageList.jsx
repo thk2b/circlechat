951ca623a48395fdcb68292a320859ac683ed329
Theo <theokobb@Theos-Air.fios-router.home>
2018-01-04 10:01:05 -0500

    user autoscroll-react. Remove src/components/autoscroll

--- client/src/messages/components/MessageList.jsx
+++ client/src/messages/components/MessageList.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 
+import Autoscroll from 'autoscroll-react'
 import Message from './Message'
 import './styles/MessageList.css'
 
-export default ({ messages }) => (
-    <ul className="MessageList">{ 
-        messages.reverse().map(
-            message => <Message 
-                key={ message.id } 
-                {...message}
-            />
+class MessageList extends React.Component{
+    render(){
+        const { messages, ...rest } = this.props
+        return (
+            <ul 
+                className="MessageList" 
+                { ...rest }
+            >{ 
+                messages.map(
+                    message => <Message 
+                        key={ message.id } 
+                        {...message}
+                    />
+                )
+            }</ul>
         )
-    }</ul>
-)
+    }
+}
+
+export default Autoscroll(MessageList)
