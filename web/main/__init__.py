e75e8cb49d8444c94db81d7a0546f03ce929d553
Theo <theokobb@Theos-MacBook-Air.local>
2018-01-21 11:19:43 -0500

    WEB: add dependency versions

--- web/main/__init__.py
+++ web/main/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask
-from flask_cors import CORS
+# from flask_cors import CORS
 from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
 import redis
