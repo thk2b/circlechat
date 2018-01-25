122525bd2e7909d897033f84d67fd1d2b0f14d7b
Theo <theokobb@Theos-MacBook-Air.local>
2018-01-24 20:56:46 -0500

    node dev setup

---
+++ web/app/index.js
@@ -0,0 +1,6 @@
+// const bodyParser = require('body-parser')
+// const express = require('express')
+// const io = require('socket.io')
+// const sequelize = require('sequelize')
+
+console.log(process.env.DATABASE_URL)
