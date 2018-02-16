cf6b6a310ff796abd33957cef5246468b56bf0a7
Theo <theokobb@Theos-MacBook-Air.local>
2018-02-16 14:37:44 -0500

    [web] refactor `auth.service`

--- web/app/modules/auth/service.js
+++ web/app/modules/auth/service.js
@@ -6,11 +6,11 @@ const jwt = require('jsonwebtoken')
 const config = require('../../config')
 const db = require('../../db')
 
-module.exports = {
-    /**
-     * Create database tables
-     */
-    init: () => new Promise((resolve, reject) => {
+/**
+ * Create database tables
+ */
+function init(){
+    return new Promise((resolve, reject) => {
         db.any(`
             CREATE TABLE auth (
                 "userId" VARCHAR(256) UNIQUE NOT NULL,
@@ -19,44 +19,53 @@ module.exports = {
                 PRIMARY KEY ("userId")
             )
         ;`)
-            .then(() => resolve())
-            .catch(e => reject(e))
-    }),
-    /**
-     * Drop database tables
-     */
-    drop: () => new Promise((resolve, reject) => {
+        .then(() => resolve())
+        .catch(e => reject(e))
+    })
+}
+
+/**
+ * Drop database tables
+ */
+function drop(){
+    return new Promise((resolve, reject) => {
         db.none(`DROP TABLE IF EXISTS auth`)
-            .then(() => resolve())
-            .catch(e => reject(e))
-    }),
-    /**
-     * Register a user
-     */
-    register: ({ userId, email, pw }) => new Promise((resolve, reject) => {
+        .then(() => resolve())
+        .catch(e => reject(e))
+    })
+}
+
+/**
+ * Register a user
+ */
+function register({ userId, email, pw }){
+    return new Promise((resolve, reject) => {
         bcrypt.hash(pw, 10)
-            .then(hashedPw => db.one(SQL`
-                INSERT INTO auth ("userId", email, pw)
-                VALUES (${userId}, ${email}, ${hashedPw})
-                RETURNING "userId"
-            ;`))
-            .then(id => resolve(id))
-            .catch(e => {
-                switch(e.code){
-                    case '23505': // violates unique constraint
-                        return reject({ code: 409, message: `user id or email already in use`})
-                    case '23502': // violates null constraint
-                        return reject({ code: 422, message: `incomplete credentials`})
-                    default:
-                        console.error(e)
-                        return reject({ code: 500, message: 'internal server error', data: e})
-                }
-            })
-    }),
-    /**
-     * Login a user
-     */
-    login: ({ userId, email, pw }) => new Promise((resolve, reject) => {
+        .then(hashedPw => db.one(SQL`
+            INSERT INTO auth ("userId", email, pw)
+            VALUES (${userId}, ${email}, ${hashedPw})
+            RETURNING "userId"
+        ;`))
+        .then(id => resolve(id))
+        .catch(e => {
+            switch(e.code){
+                case '23505': // violates unique constraint
+                    return reject({ code: 409, message: `user id or email already in use`})
+                case '23502': // violates null constraint
+                    return reject({ code: 422, message: `incomplete credentials`})
+                default:
+                    console.error(e)
+                    return reject({ code: 500, message: 'internal server error', data: e})
+            }
+        })
+    })
+}
+
+/**
+ * Login a user
+ */
+function login({ userId, email, pw }){
+    return new Promise((resolve, reject) => {
         db.one(SQL`
             SELECT "userId" as id, pw as "hashedPw" FROM auth 
             WHERE "userId"=${userId} or email=${email}
@@ -72,17 +81,32 @@ module.exports = {
             })
         })
         .catch(e => reject({ code: 401, message: 'invalid credentials' }))
-    }),
-    /**
-     * update credentials with keys
-     */
-    update: ({ id, keys }) => new Promise((resolve, reject) => {
-        reject(new Error('not implemented'))
-    }),
-    /**
-     * delete credentials
-     */
-    delete: id => new Promise((resolve, reject) => {
+    })
+}
+
+/**
+ * update credentials with keys
+ */
+function update({ id, keys }){
+    return new Promise((resolve, reject) => {
         reject(new Error('not implemented'))
     })
+}
+
+/**
+ * delete credentials
+ */
+function remove(id){
+    return new Promise((resolve, reject) => {
+        reject(new Error('not implemented'))
+    })
+}
+
+module.exports = {
+    init,
+    drop,
+    register,
+    login,
+    update,
+    remove
 }
