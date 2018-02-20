a2e9ee698d28d3ead89a5e7e38e983b6ed81f19f
Theo <theokobb@Theos-MacBook-Air.local>
2018-02-20 17:38:30 -0500

    [client] connect group to store and api

--- client/src/group/Group.jsx
+++ client/src/group/Group.jsx
@@ -1,19 +1,40 @@
 import React from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+import { push } from 'react-router-redux'
 
-const mapState = () => {
-    return {}
+import { getAll as getAllProfiles } from '../profiles'
+
+
+const mapState = ({ auth, profiles }) => {
+    return {
+        token: auth.token,
+        profiles: Object.keys(profiles.data).reduce(
+            (arr, id) => profiles.data[id]
+        ,[])
+    }
 }
 const mapDispatch = dispatch => {
-    return {}
+    return bindActionCreators({ getAllProfiles, push }, dispatch)
 }
 
-export default ({ push, token }) => {
-    if(! token )
-    return (
-        <div>
-        group
+class Group extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            ui: { isUsersMenuOpen: false }
+        }
+    }
+    componentDidMount(){
+        if(!this.props.token ) return this.props.push('/login')
+        if(this.props.profiles === [] || this.props.profiles.length === 1) console.log(true)
+    }
+    
+    render() {
+        return <div>
+            {JSON.stringify(this.props)}
         </div>
-    )
+    }
 }
+
+export default connect(mapState, mapDispatch)(Group)
