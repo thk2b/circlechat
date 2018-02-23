9e680e946e8f9e761d2580e5c3bc6283d8087004
Theo <theokobb@Theos-MacBook-Air.local>
2018-02-23 11:25:25 -0500

    [web, client] harmonize socket data, api endpoint and reducer

--- web/app/modules/ping/events.js
+++ web/app/modules/ping/events.js
@@ -1,4 +1,4 @@
-module.exports = function(socket, io){
+module.exports = function(socket, io, { meta, data }){
     if(socket.userId){
         return socket.emit('/pong', { meta: { status: 200 }})
     }
