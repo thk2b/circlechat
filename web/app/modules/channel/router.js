617e026e16a65002155d58938cbdd45b3cff3fe0
Theo <theokobb@Theos-MacBook-Air.local>
2018-03-06 16:23:03 -0500

    [web] finish channel routes

--- web/app/modules/channel/router.js
+++ web/app/modules/channel/router.js
@@ -4,6 +4,11 @@ const channel = require('./service')
 
 const r = new Router()
 
+const validateQueryParams = query => {
+    if(query.id) return Promise.resolve()
+    return Promise.reject({ status: 422, message: 'invalid query parameters'})
+}
+
 r.route('/')
     .post((req, res, next) => {
         channel.create(req.userId, req.body)
@@ -16,19 +21,41 @@ r.route('/')
         })
         .catch(next)
     })
-    .get((req, res) => {
-
+    .get((req, res, next) => {
+        validateQueryParams(req.query)
+        .then(() => channel.get(req.userId, req.query.id))
+        .then(channel => res.status(200).json({ channel }))
+        .catch(next)
     })
-    .put((req, res) => {
-
+    .put((req, res, next) => {
+        validateQueryParams(req.query)
+        .then(() => channel.update(req.userId, req.query.id, req.body))
+        .then( data => {
+            res.status(202).json(data)
+            res.locals.socket && res.locals.socket.broadcast.emit('/profile', {
+                meta: { type: 'PUT', status: 202 },
+                data
+            })
+        })
+        .catch(next)
     })
-    .delete((req, res) => {
-
+    .delete((req, res, next) => {
+        validateQueryParams(req.query)
+        .then(() => channel.remove(req.userId, req.query.id))
+        .then(() => {
+            res.status(202).end()
+            res.locals.socket && res.locals.socket.broadcast.emit('/channel', {
+                meta: { type: 'DELETE', status: 202 }
+            })
+        })
+        .catch(next)
     })
 
 r.route('/all')
-    .get((req, res) => {
-
+    .get((req, res, next) => {
+        channel.getAll(req.userId)
+        .then( channels => res.status(200).json(channels))
+        .catch(next)
     })
 
 module.exports = r
--- web/app/modules/channel/test/router.test.js
+++ web/app/modules/channel/test/router.test.js
@@ -139,13 +139,194 @@ describe(API_URL, function(){
                 .set('Content-Type', 'application/json')
                 .set('Authorization', 'Bearer ' + token1)
                 .send(channel1)
-                .end(e => {
+                .end((e, res) => {
                     if(e) done(e)
+                    channel1 = res.body.channel
                 })
         })
     })
-    describe(`GET ${API_URL}`, function(){})
-    describe(`GET ${API_URL}/all`, function(){})
-    describe(`PUT ${API_URL}`, function(){})
-    describe(`DELETE ${API_URL}`, function(){})
+    describe(`GET ${API_URL}`, function(){
+        it('should send a 422 when invalid query params are sent', function(done){
+            request(server)
+                .get(API_URL)
+                .query({wrong: 123})
+                .set('Content-Type', 'application/json')
+                .set('Authorization', 'Bearer ' + token)
+                .expect(422)
+                .end(done)
+        })
+        it('should not send a channel that does not exist', function(done){
+            request(server)
+                .get(API_URL)
+                .query({id: 123})
+                .set('Content-Type', 'application/json')
+                .set('Authorization', 'Bearer ' + token)
+                .expect(404)
+                .end(done)
+        })
+        it('should send a channel when authorized and authenticated', function(done){
+            request(server)
+                .get(API_URL)
+                .query({ id: channel.id })
+                .set('Content-Type', 'application/json')
+                .set('Authorization', 'Bearer ' + token)
+                .expect(200)
+                .end((e, res) => {
+                    if(e) return done(e)
+                    expect(res.body.channel).to.deep.equal(channel)
+                    done()
+                })
+        })
+        describe(`GET ${API_URL}/all`, function(){
+            it('should not send all channels when unauthenticated', function(done){
+                request(server)
+                    .get(API_URL + '/all')
+                    .set('Content-Type', 'application/json')
+                    .expect(401)
+                    .end(done)
+            })
+            it('should not send all channels with an invalid token', function(done){
+                request(server)
+                    .get(API_URL + '/all')
+                    .set('Authorization', 'Bearer 0' + token)
+                    .set('Content-Type', 'application/json')
+                    .expect(401)
+                    .end(done)
+            })
+            it('should send all channels when authorized and authenticated', function(done){
+                request(server)
+                    .get(API_URL + '/all')
+                    .set('Authorization', 'Bearer ' + token)
+                    .set('Content-Type', 'application/json')
+                    .expect(200)
+                    .end((e, res) => {
+                        if(e) return done(e)
+                        expect(res.body[channel.id]).to.not.be.undefined
+                        expect(res.body[channel1.id]).to.not.be.undefined
+                        done()
+                    })
+            })
+        })
+    })
+    describe(`PUT ${API_URL}`, function(){
+        it('should not update a channel when unauthenticated', function(done){
+            request(server)
+                .put(API_URL)
+                .query({ id: channel.id })
+                .send({ name: 'new name' })
+                .set('Content-Type', 'application/json')
+                .expect(401)
+                .end(done)
+        })
+        it('should not update a channel with an invalid token', function(done){
+            request(server)
+                .put(API_URL)
+                .query({ id: channel.id })
+                .send({ name: 'new name' })
+                .set('Authorization', 'Bearer 0' + token)
+                .set('Content-Type', 'application/json')
+                .expect(401)
+                .end(done)
+        })
+        it('should not update a channel created by another user', function(done){
+            request(server)
+                .put(API_URL)
+                .query({ id: channel1.id })
+                .send({ name: 'new name' })
+                .set('Authorization', 'Bearer ' + token)
+                .set('Content-Type', 'application/json')
+                .expect(403)
+                .end(done)
+        })
+        it('should not update a channel when data is invalid', function(done){
+            request(server)
+                .put(API_URL)
+                .query({ id: channel.id })
+                .send({ wrong: 'key' })
+                .set('Authorization', 'Bearer ' + token)
+                .set('Content-Type', 'application/json')
+                .expect(422)
+                .end(done)
+        })
+        it('should update a channel when authorized and authenticated', function(done){
+            request(server)
+                .put(API_URL)
+                .query({ id: channel.id })
+                .send({ name: 'new name' })
+                .set('Authorization', 'Bearer ' + token)
+                .set('Content-Type', 'application/json')
+                .expect(202)
+                .end((e, res) => {
+                    if(e) return done(e)
+                    channel = {...channel, ...res.body}
+                    expect(res.body.id).to.not.be.undefined
+                    expect(res.body.updatedAt).to.not.be.undefined
+                    expect(res.body.name).to.equal('new name')
+                    
+                    done()
+                })
+        })
+        it('should notify websocket clients', function(done){
+            socket.once('/profile', ({ meta, data }) => {
+                expect(meta).to.deep.equal({
+                    status: 202, type: 'PUT'
+                })
+                expect(data.name).to.equal('new name1')
+                done()
+            })
+            request(server)
+                .put(API_URL)
+                .query({ id: channel1.id })
+                .send({ name: 'new name1' })
+                .set('Authorization', 'Bearer ' + token1)
+                .set('Content-Type', 'application/json')
+                .expect(202)
+                .end((e, res) => {
+                    if(e) return done(e)
+                    channel1 = {...channel1, ...res.body}
+                })
+        })
+    })
+    describe(`DELETE ${API_URL}`, function(){
+        it('should not delete a channel when unauthenticated', function(done){
+            request(server)
+                .delete(API_URL)
+                .query({ id: channel.id })
+                .set('Content-Type', 'application/json')
+                .expect(401)
+                .end(done)
+        })
+        it('should not delete a channel created by another user', function(done){
+            request(server)
+                .delete(API_URL)
+                .query({ id: channel1.id })
+                .set('Authorization', 'Bearer ' + token)
+                .set('Content-Type', 'application/json')
+                .expect(403)
+                .end(done)
+        })
+        it('should delete a channel when authorized and authenticated', function(done){
+            request(server)
+                .delete(API_URL)
+                .query({ id: channel.id })
+                .set('Authorization', 'Bearer ' + token)
+                .set('Content-Type', 'application/json')
+                .expect(202)
+                .end(done)
+        })
+        it('should notify websocket clients', function(done){
+            socket.once('/channel', ({ meta }) => {
+                expect(meta).to.deep.equal({
+                    status: 202, type: 'DELETE'
+                })
+            })
+            request(server)
+                .delete(API_URL)
+                .query({ id: channel1.id })
+                .set('Authorization', 'Bearer ' + token1)
+                .set('Content-Type', 'application/json')
+                .expect(202)
+                .end(done)
+        })
+    })
 })
