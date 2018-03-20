a76cc86968c44e5d12de13e1080c0c18939d08e8
Theo <theokobb@gmail.com>
2018-03-20 11:35:57 -0400

    [client] fix notification pill

--- client/src/app/containers/ChannelsList/ChannelListItem.jsx
+++ client/src/app/containers/ChannelsList/ChannelListItem.jsx
@@ -3,11 +3,15 @@ import React from 'react'
 import ListItem from 'material-ui/List/ListItem'
 import { Link, NotificationPill } from '../../lib'
 
+import css from './ChannelListItem.css'
+
 export default ({onClick, name, notifications, hasMore }) => {
     return <ListItem
         onClick={onClick}
     >
-        <Link>{name}</Link>
-        <NotificationPill count={notifications} hasMore={hasMore && notifications >= 20} />
+        <div className={css.ChannelListItem}>
+            <Link>{name}</Link>
+            <NotificationPill count={notifications} hasMore={hasMore && notifications >= 20} />
+        </div>
     </ListItem>
 }
