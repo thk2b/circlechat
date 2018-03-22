6e90f290b23c04747b97a15df9d18985d7bc61d2
Theo <theokobb@gmail.com>
2018-03-22 18:54:58 -0400

    [client] fix status color

--- client/src/app/lib/Status/index.jsx
+++ client/src/app/lib/Status/index.jsx
@@ -2,25 +2,22 @@ import React from 'react'
 import classNames from 'classnames'
 import { withStyles } from 'material-ui/styles'
 
-const styles = theme => {
-    console.log(theme)
-    return {
-        root: {
-            display: 'inline-block',
-            width: '18px',
-            height: '18px',
-            borderRadius: '50%',
-            marginLeft: '1rem',
-            verticalAlign: 'bottom',
-        },
-        online: {
-            backgroundColor: theme.palette.primary.main,
-        },
-        offline: {
-            backgroundColor: theme.palette.grey[500]
-        }
+const styles = theme => ({
+    root: {
+        display: 'inline-block',
+        width: '18px',
+        height: '18px',
+        borderRadius: '50%',
+        marginLeft: '1rem',
+        verticalAlign: 'bottom',
+    },
+    online: {
+        backgroundColor: theme.palette.primary.main,
+    },
+    offline: {
+        backgroundColor: theme.palette.grey[500]
     }
-}
+})
 
 export default withStyles(styles)(({ classes, status = 'INVISIBLE' }) => {
     if(status === 'INVISIBLE') return null
