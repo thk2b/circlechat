4ae01ca7c6ca05146fe87186918799629a8cfb62
Theo <theokobb@gmail.com>
2018-03-22 17:44:56 -0400

    [client] notificationpill

--- client/src/app/lib/NotificationPill/index.jsx
+++ client/src/app/lib/NotificationPill/index.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { withStyles } from 'material-ui/styles'
 
-import css from './NotificationPill.css'
+const styles = theme => ({
+    root: {
+        backgroundColor: theme.palette.primary.dark,
+        borderRadius: '50%',
+        width: '18px',
+        height: '18px',
+        textAlign: 'center',
+        color: theme.palette.text.primary,
+        lineHeight: '1.1rem'
+    }
+})
 
-export default ({ count, hasMore }) => {
+export default withStyles(styles)(({ classes, count, hasMore }) => {
     if(! count ) return null
-    return <span className={css.NotificationPill} >
+    return <span className={classes.root} >
         { hasMore && '+'}{ count }
     </span>
-}
+})
