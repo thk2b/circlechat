4dbc6d1054fa1ce992dc956282f73e7d99e4023e
Theo <theokobb@gmail.com>
2018-04-17 21:04:58 -0400

    remove old middleware

--- client/src/store/middleware/index.js
+++ client/src/store/middleware/index.js
@@ -1,4 +1,2 @@
-export { default as apiMiddleware } from './apiMiddleware'
 export { default as deviceMiddleware } from './deviceMiddleware'
-export { default as notificationsMiddleware } from './notificationsMiddleware'
 export { default as socketIoMiddleware} from './websocket'
