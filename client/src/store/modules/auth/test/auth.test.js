49c1ec796d69d0d2740707029f192d63182dbcd6
Theo <theokobb@gmail.com>
2018-04-19 10:17:43 -0400

    test auth register

--- client/src/store/modules/auth/test/auth.test.js
+++ client/src/store/modules/auth/test/auth.test.js
@@ -47,7 +47,31 @@ describe('auth network actions', () => {
             done()
         })
     })
-    test.skip('auth.register', () => {})
+    test('auth.register success', done => {
+        const data = { userId: 'tester', pw: 123 }
+        mock.onPost('/auth', data).reply(201)
+
+        store.dispatch(actions.register(data))
+        expect(store.getState().loading.auth.register).toBe(true)
+        runAsync(done, () => {
+            const state = store.getState()
+            expect(state.loading.auth).toEqual({ login: false, register: false })
+            done()
+        })
+    })
+    test('auth.register error', done => {
+        const data = { message: 'test error' }
+        mock.onPost('/auth').reply(422, data)
+
+        store.dispatch(actions.register({ userId: 'tester', pw: 123 }))
+        expect(store.getState().loading.auth.register).toBe(true)
+        runAsync(done, () => {
+            const state = store.getState()
+            expect(state.loading.auth).toEqual({ login: false, register: false })
+            expect(state.errors.auth).toEqual({ login: null, register: data })
+            done()
+        })
+    })
     test.skip('auth.logout', () => {
 
     })
--- client/src/store/modules/errors/index.js
+++ client/src/store/modules/errors/index.js
@@ -4,7 +4,7 @@ const { reducer, actions } = HashMap('errors', {
     channels: { all: null, new: null },
     messages: { all: null, new: null },
     profiles: { all: null, new: null },
-    auth: null
+    auth: { login: null, register: null }
 })
 
 export { actions }
