b0ad64541f5c96b7c6f05de549b7900d169cb0af
Theo <theokobb@gmail.com>
2018-05-01 17:59:13 -0400

    Implement Sidebars

---
+++ client/src/_app/containers/Channel/ChannelsSidebar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+import { push } from 'react-router-redux'
+
+const mapState = ({ channels }) => {
+    return {
+        channels: Object.values(channels)
+    }
+}
+
+const mapDispatch = dispatch => {
+    return {
+        goToChannel = id => dispatch(push(`channel/${id}`))
+    }
+}
+
+const ChannelsSidebar = ({ channels, goToChannel }) => {
+    return (
+        <ul
+            className='sidebar'
+        >{channels.map(
+            channel => <li
+                key={channel.id}
+                onClick={e => goToChannel(channel.id)}
+            >
+                {channel.name}
+            </li>
+        )}</ul>
+    )
+}
+
+export default connect(mapState, mapDispatch)(ChannelsSidebar)
---
+++ client/src/_app/containers/Profile/ProfilesSidebar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+import { push } from 'react-router-redux'
+
+const mapState = ({ profiles }) => {
+    return {
+        profiles: Object.values(profiles)
+    }
+}
+
+const mapDispatch = dispatch => {
+    return {
+        goToProfile = id => dispatch(push(`channel/${id}`))
+    }
+}
+
+const ProfilesSidebar = ({ profiles, goToProfile }) => {
+    return (
+        <ul
+            className='sidebar'
+        >{profiles.map(
+            profile => <li
+                key={profile.id}
+                onClick={e => goToProfile(profile.id)}
+            >
+                {profile.name}
+            </li>
+        )}</ul>
+    )
+}
+
+export default connect(mapState, mapDispatch)(ProfilesSidebar)
