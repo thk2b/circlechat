f9ddcc67cfb7280659760cba98d9be893584cec9
Theo <theokobb@gmail.com>
2018-05-03 22:15:18 -0400

    Settings and tooltip

--- client/src/_app/containers/Nav/Settings.jsx
+++ client/src/_app/containers/Nav/Settings.jsx
@@ -1,24 +1,76 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import styled from 'styled-components'
+import Popover from '@thk2b/oui/lib/Popover'
+import MdSettings from 'react-icons/lib/md/settings'
 
-const mapState = ({}) => {
+import { setTheme } from '../../../store/modules/theme'
+
+const mapState = ({ theme }) => {
     return {
-        
+        theme: theme.active,
+        themes: ['light', 'dark']
     }
 }
 
 const mapDispatch = dispatch => {
     return {
-
+        setTheme: theme => dispatch(setTheme(theme))
     }
 }
 
-const Settings = ({ }) => {
-    return (
-        <div>
-            settings
-        </div>
-    )
+const IconContainer = styled.div`
+    padding: 10px;
+`
+const MenuContainer= styled.aside`
+    z-index: 100;
+    width: 250px;
+    & h3 {
+        text-align: center;
+        padding: 10px;
+    }
+    & span:not(:first-of-type) {
+        border-top: 1px solid black
+    }
+`
+
+const MenuItemContainer = styled.span`
+    padding: 10px;
+    display: flex;
+    justify-content: space-around;
+`
+
+const Settings = ({ theme, themes, setTheme }) => {
+    return <Popover
+        zIndex={1}
+        Component={() => <IconContainer>
+            <MdSettings
+                width='100%'
+                size={32}
+                color='#d9d9d9'
+            />
+        </IconContainer>}
+        position={{bottom: 0, left: '100%'}}
+    >
+        <MenuContainer>
+            <h3>settings</h3>
+            <MenuItemContainer>
+                <p>theme</p>
+                <select
+                    onChange={({ target }) => setTheme(target.value)}
+                    value={theme}
+                >
+                    {themes.map(
+                        theme => <option
+                            key={theme}
+                            value={theme}
+                        >{theme}</option>
+                    )}
+                </select>
+            </MenuItemContainer>
+        </MenuContainer>
+    </Popover>
+       
 }
 
 export default connect(mapState, mapDispatch)(Settings)
--- client/src/_app/containers/Nav/index.jsx
+++ client/src/_app/containers/Nav/index.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import styled from 'styled-components'
 
 import OwnProfileLink from './OwnProfileLink'
 import Settings from './Settings'
 
+const Nav = styled.nav`
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: space-between;
+    align-items: center;
+`
+
 export default () => {
     return (
-        <nav>
+        <Nav>
             <OwnProfileLink/>
             <Settings/>
-        </nav>
+        </Nav>
     )
 }
