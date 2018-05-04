c5b1193eceec79cd812b4375285ee11fcc1ad42c
Theo <theokobb@gmail.com>
2018-05-04 13:34:12 -0400

    Remove mesage popover

--- client/src/_app/containers/Channel/Chat/Message.jsx
+++ client/src/_app/containers/Channel/Chat/Message.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import styled from 'styled-components'
+import { bindActionCreators } from 'redux'
 
+import { messagesActions } from '../../../../store/modules/messages'
 import { push } from 'react-router-redux'
 import Time from '../../../lib/Time'
+import Menu from '../../../lib/Menu'
 
 const mapState = ({ profiles }, { message }) => {
     return {
@@ -13,13 +16,19 @@ const mapState = ({ profiles }, { message }) => {
 
 const mapDispatch = dispatch => {
     return {
-        push: url => dispatch(push(url))
+        ...bindActionCreators({
+            update: messagesActions.update,
+            remove: messagesActions.remove,
+            push
+        }, dispatch)
     }
 }
 
-const mergeProps = ( { profile }, { push }, ownProps) => {
+const mergeProps = ( { profile }, { push, update, remove }, ownProps) => {
     return {
-        goToProfile: () => push(`/profile/${profile.id}`),
+        onGoToProfile: () => push(`/profile/${profile.id}`),
+        onUpdateMessage: text => update(ownProps.id, { text }),
+        onDeleteMessage: () => remove(ownProps.id),
         profile,
         ...ownProps
     }
@@ -34,24 +43,33 @@ const Article = styled.article`
     border-radius: 3px;
 `
 
-const Message = ({ message, profile, goToProfile }) => {
+const MetaDataContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+`
+
+const Message = ({ message, profile, onGoToProfile, onDeleteMessage, onUpdateMessage }) => {
     const deleted = message === null
     const updated = message.createdAt !== message.updatedAt
     return <Li>
         <Article>
             <p>{deleted? '[deleted]' : message.text}</p>
-            <a
-                rel="noopener"
-                href=""
-                onClick={e => {
-                    e.preventDefault()
-                    goToProfile()
-                }}
-            >by {profile.name}</a>
-            <p>sent <Time since={message.createdAt}/></p>
-            {updated &&
-                <p>{deleted? 'deleted': 'updated'} <Time since={message.updatedAt}/></p>
-            }
+            <MetaDataContainer>
+                <a
+                    rel="noopener"
+                    href=""
+                    onClick={e => {
+                        e.preventDefault()
+                        onGoToProfile()
+                    }}
+                >by {profile.name}</a>
+                <p>sent <Time since={message.createdAt}/></p>
+                {updated &&
+                    <p>{deleted? 'deleted': 'updated'} <Time since={message.updatedAt}/></p>
+                }
+            </MetaDataContainer>
         </Article>
     </Li>
 }
