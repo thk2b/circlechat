a41032827e1928f7563f8b3509ecef4de8ff4e4d
Theo <theokobb@gmail.com>
2018-05-06 15:08:09 -0400

    Icon hover effects

--- client/src/app/lib/LabeledIcon.jsx
+++ client/src/app/lib/LabeledIcon.jsx
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 const Container = styled.div`
     text-align: center;
     & label {
+        display: block;
         font-size: 12px;
         text-transform: uppercase;
     }
